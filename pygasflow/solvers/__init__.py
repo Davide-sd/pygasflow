d049361354c6a87a3d85f5fa665ed8ffc3853b90
Davide Sandonà <sandona.davide@gmail.com>
2024-11-04 15:06:25 +0100

    added normal_shockwave_solver

--- pygasflow/solvers/__init__.py
+++ pygasflow/solvers/__init__.py
@@ -1,5 +1,12 @@
 from .isentropic import isentropic_solver
 from .fanno import fanno_solver
 from .rayleigh import rayleigh_solver
-from .shockwave import shockwave_solver, conical_shockwave_solver
-from .de_laval import De_Laval_Solver, find_shockwave_area_ratio
+from .shockwave import (
+    shockwave_solver,
+    conical_shockwave_solver,
+    normal_shockwave_solver
+)
+from .de_laval import (
+    De_Laval_Solver,
+     find_shockwave_area_ratio
+)
