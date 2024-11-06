070afda08c7f7b7f901a522b1f24f8ded1e6072c
Davide Sandonà <sandona.davide@gmail.com>
2024-11-06 19:18:44 +0100

    added diagram function

--- pygasflow/interactive/__init__.py
+++ pygasflow/interactive/__init__.py
@@ -1,5 +1,7 @@
 from pygasflow.interactive.compressible import compressible_app
+from pygasflow.interactive.diagrams import diagram
 
 __all__ = [
-    "compressible_app"
+    "compressible_app",
+    "diagram",
 ]
--- pygasflow/interactive/diagrams/__init__.py
+++ pygasflow/interactive/diagrams/__init__.py
@@ -1,9 +1,56 @@
+from bokeh.plotting import show
 from pygasflow.interactive.diagrams.isentropic import IsentropicDiagram
 from pygasflow.interactive.diagrams.fanno import FannoDiagram
 from pygasflow.interactive.diagrams.rayleigh import RayleighDiagram
 from pygasflow.interactive.diagrams.normal_shock import NormalShockDiagram
 from pygasflow.interactive.diagrams.oblique_shock import ObliqueShockDiagram
 from pygasflow.interactive.diagrams.conical_shock import ConicalShockDiagram
+
+def diagram(select="isentropic", interactive=False, **params):
+    """Create the selected diagram.
+
+    Parameters
+    ----------
+    select : str
+        Available options are: isentropic, fanno, rayleigh, normal_shock,
+        oblique_shock, conical_shock
+    interactive : bool
+        If True, an Holoviz's panel object will be returned.
+    **params :
+        Keyword arguments passed to the diagram component for further
+        customization.
+
+    Returns
+    -------
+    diagram : Column or None
+        If ``interactive=True``, a panel's ``Column`` object will be
+        returned, containing the widgets and the figure. It will be
+        automatically rendered on Jupyter Notebook/Lab. If the interpreter
+        is unable to render it, execute the ``.show()`` method on the returned
+        object.
+        If ``interactive=False``, it returns None. The figure will be
+        visualized on the screen (either a Jupyter Notebook/Lab's cell or in a
+        new browser window).
+    """
+    mapping = {
+        "isentropic": IsentropicDiagram,
+        "fanno": FannoDiagram,
+        "rayleigh": RayleighDiagram,
+        "normal_shock": NormalShockDiagram,
+        "oblique_shock": ObliqueShockDiagram,
+        "conical_shock": ConicalShockDiagram,
+    }
+    if not select in mapping:
+        raise ValueError(
+            "``select`` must be one of the following"
+            f" options: {list(mapping.keys())}."
+            f" Instead, '{select}' was received."
+        )
+    diagram = mapping[select](**params)
+    if interactive:
+        return diagram.servable()
+    show(diagram.figure)
+
 
 __all__ = [
     "IsentropicDiagram",
@@ -12,4 +59,5 @@ __all__ = [
     "NormalShockDiagram",
     "ObliqueShockDiagram",
     "ConicalShockDiagram",
+    "diagram"
 ]
