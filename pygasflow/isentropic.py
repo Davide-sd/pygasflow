inserted correct numerical values for tests + updated pictures

pygasflow/isentropic.py
@@ -34,7 +34,11 @@ def critical_velocity_ratio(M, gamma=1.4):
     out : ndarray
         Critical velocity ratio U/U*.
     """
-    return M * ((gamma + 1) / (2 + (gamma - 1) * M**2)) ** 0.5
+    # need to deal with division by 0
+    ratio = np.zeros_like(M)
+    idx = M != 0
+    ratio[idx] = M[idx] * ((gamma + 1) / (2 + (gamma - 1) * M[idx]**2)) ** 0.5
+    return ratio
 
 
 @check

tests/solvers/test_isentropic.py
@@ -31,7 +31,7 @@ def test_input_mach():
         [0.24192491, 0.05153250],   # Critical Pressure Ratio
         [0.36288736, 0.12024251],   # Critical Density Ratio
         [0.66666666, 0.42857142],   # Critical Temperature Ratio
-        [2.35151015, 2.82810385],   # Critical Velocity Ratio
+        [1.63299316, 1.96396101],   # Critical Velocity Ratio
         [1.68749999, 4.23456790],   # Critical Area Ratio
         [29.9999999, 19.4712206],   # Mach Angle
         [26.3797608, 49.7573467],   # Prandtl-Meyer Angle
@@ -73,7 +73,7 @@ def test_input_parameters_supersonic(param_name, value):
         0.05153250, # Critical Pressure Ratio P/P*
         0.12024251, # Critical Density Ratio rho/rho*
         0.42857142, # Critical Temperature Ratio T/T*
-        2.82810385, # Critical Velocity Ratio U/U*
+        1.96396101, # Critical Velocity Ratio U/U*
         4.23456790, # Critical Area Ratio A/A*
         19.4712206, # Mach Angle
         49.7573467, # Prandtl-Meyer Angle
@@ -103,7 +103,7 @@ def test_input_parameters_subsonic(param_name, value):
         1.59577557, # Critical Pressure Ratio P/P*
         1.39630363, # Critical Density Ratio rho/rho*
         1.14285714, # Critical Temperature Ratio T/T*
-        0.76971237, # Critical Velocity Ratio U/U*
+        0.5345224838248488, # Critical Velocity Ratio U/U*
         1.33984375, # Critical Area Ratio A/A*
         np.nan, # Mach Angle
         np.nan, # Prandtl-Meyer Angle

tests/test_decorators.py
@@ -108,7 +108,7 @@ def test_raise_error_isentropic():
     # the function and either produce errors or produce random junk results
     func(TypeError, ise.critical_velocity_ratio, -1, nocheck=True)  # int is not subscriptable
     func(TypeError, ise.critical_velocity_ratio, 2, gamma=0.9, nocheck=True)  # int is not subscriptable
-    assert np.all(ise.critical_velocity_ratio.__no_check__([2, 3, -1]) - np.asarray([2, 2, 2]) == 0)
+    assert np.all(ise.critical_velocity_ratio.__no_check__([2, 3, -1]) - np.asarray([1, 1, 1]) == 0)
 
 
 def test_raise_error_fanno():

tests/test_isentropic.py
@@ -3,8 +3,10 @@ from pygasflow.isentropic import (
     sonic_density_ratio,
     sonic_pressure_ratio,
     sonic_temperature_ratio,
-    sonic_sound_speed_ratio
+    sonic_sound_speed_ratio,
+    critical_velocity_ratio,
 )
+import pytest
 
 
 def test_sonic_conditions():
@@ -19,3 +21,20 @@ def test_sonic_conditions():
     assert np.allclose(sonic_pressure_ratio(gammas), [1.89292916, 1.953125])
     assert np.allclose(sonic_temperature_ratio(gammas), [1.2, 1.25])
     assert np.allclose(sonic_sound_speed_ratio(gammas), [1.09544512, 1.11803399])
+
+
+@pytest.mark.parametrize("m, gamma, expected_vel_ratio", [
+    (2, 1.4, 1.63299316),
+    ([2, 3], 1.4, [1.63299316, 1.96396101]),
+    (0.5, 1.4, 0.5345224838248488),
+    (1, 1.4, 1),
+    (0, 1.4, 0),
+    (2, 1.2, 1.77281052),
+    ([2, 3], 1.2, [1.77281052, 2.28265773]),
+    (0.5, 1.2, 0.5179697702828123),
+    (1, 1.2, 1),
+    (0, 1.2, 0),
+])
+def test_critical_velocity_ratio(m, gamma, expected_vel_ratio):
+    vr = critical_velocity_ratio(m, gamma)
+    assert np.allclose(vr, expected_vel_ratio)