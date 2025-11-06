d7d00483eeb5e9e457224b3bd8b912025920a88d
Davide Sandonà <sandona.davide@gmail.com>
2025-11-06 18:04:32 +0100

    added pint support to thermal conductivity-related functions

--- pygasflow/atd/thermal_conductivity.py
+++ pygasflow/atd/thermal_conductivity.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from scipy import interpolate
 from pygasflow.utils.decorators import check_T
+from pygasflow.utils.common import _is_pint_quantity, _parse_pint_units
 
 
 @check_T
@@ -14,27 +15,83 @@ def thermal_conductivity_chapman_enskog(T, gas="O"):
     T : float or array_like
         Temperature of the air in [K]
     gas : str, optional
-        Possible values are:``'N'``, ``'O'``, ``'Ar'``, ``'He'``
+        Possible values are: ``['N', 'O', 'Ar', 'He']``
 
     Returns
     -------
     k : float or array_like
         Thermal conductivity of the gas [W / (m * K)]
 
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from pygasflow.atd import thermal_conductivity_chapman_enskog
+    >>> thermal_conductivity_chapman_enskog(300, gas="O")
+    np.float64(0.049434309555779335)
+
+    Pint quantities can be used as well:
+
+    >>> import pint
+    >>> import pygasflow
+    >>> ureg = pint.UnitRegistry()
+    >>> pygasflow.defaults.pint_ureg = ureg
+    >>> thermal_conductivity_chapman_enskog(300 * ureg.K)
+    <Quantity(0.0494343096, 'watt / meter / kelvin')>
+
+    Comparison of the thermal conductivity of different gases over a 
+    range of temperatures.
+
+    .. plot::
+       :context: close-figs
+       :include-source: True
+
+       import numpy as np
+       import matplotlib.pyplot as plt
+       from pygasflow.atd import thermal_conductivity_chapman_enskog
+       T = np.linspace(200, 1000)
+       gases = ['N', 'O', 'Ar', 'He']
+       fig, ax = plt.subplots()
+       for gas in gases:
+           ax.plot(T, thermal_conductivity_chapman_enskog(T, gas=gas), label=gas)
+       ax.legend()
+       ax.set_xlabel("Temperature [K]")
+       ax.set_ylabel("Thermal Conductivity [W / (m * K)]")
+       plt.show()
+    
     References
     ----------
 
     * "Basic of aerothermodynamics" by Ernst Heinrich, Table 13.1
     * "Transport Phenomena" by R. Byron Bird, Warren E. Stewart,
       Edwing N. Lightfoot, Table E2
+    
     """
+    available_gases = ["O", "N", "Ar", "He"]
+    if gas not in available_gases:
+        raise ValueError(
+            f"`gas` must be one of the following: {available_gases}.\n"
+            f"Instead, '{gas}' was provided."
+        )
+
+    is_pint = _is_pint_quantity(T)
+    if is_pint:
+        T = T.to("K").magnitude
+
     # path of the folder containing this file
     current_dir = os.path.dirname(os.path.realpath(__file__))
     # path of the folder containing the data of the plot
     data_dir = os.path.join(current_dir, "data")
     df1 = pd.read_csv(os.path.join(data_dir, "Table-13_1.csv"))
     df2 = pd.read_csv(os.path.join(data_dir, "Table-E2.csv"))
-    df1[df1["Gas"] == gas]
+    if gas not in df1["Gas"].values:
+        raise KeyError(
+            f"The provided gas, '{gas}', is not included in the dataframe."
+            " Please, read the documentation of this function to"
+            " understand which gases are supported."
+
+        )
+    df1 = df1[df1["Gas"] == gas]
     M = df1["M"].values[0]
     sigma = df1["sigma"].values[0] * 1e10
     eps_kappa = df1["eps_kappa"].values[0]
@@ -42,8 +99,13 @@ def thermal_conductivity_chapman_enskog(T, gas="O"):
     spline = interpolate.InterpolatedUnivariateSpline(
         df2["kT_eps"], df2["visc_thermal_cond"])
     Sigma_mu = spline(kappaT_eps)
+
     # eq (4.16)
-    return 8.3225e-02 * np.sqrt(T / M) / (sigma**2 * Sigma_mu)
+    k = 8.3225e-02 * np.sqrt(T / M) / (sigma**2 * Sigma_mu)
+
+    if is_pint:
+        k *= _parse_pint_units("W / (m * K)")
+    return k
 
 
 def thermal_conductivity_eucken(cp, R, mu):
@@ -64,14 +126,54 @@ def thermal_conductivity_eucken(cp, R, mu):
     -------
     k : float or array_like
         Thermal conductivity of the gas [W / (m * K)]
+    
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from pygasflow.atd import thermal_conductivity_eucken
+    >>> cp, R, mu = 1010, 288, 1.863e-05
+    >>> thermal_conductivity_eucken(cp, R, mu)
+    np.float64(0.0255231)
+
+    Pint quantities can be used as well:
+
+    >>> import pint
+    >>> import pygasflow
+    >>> ureg = pint.UnitRegistry()
+    >>> pygasflow.defaults.pint_ureg = ureg
+    >>> cp = 1010 * ureg.J / (ureg.kg * ureg.K)
+    >>> R = 288 * ureg.J / (ureg.kg * ureg.K)
+    >>> mu = 1.863e-05 * ureg.kg / (ureg.m * ureg.s)
+    >>> thermal_conductivity_eucken(cp, R, mu)
+    <Quantity(0.0255231, 'watt / meter / kelvin')>
 
     References
     ----------
 
     "Basic of Aerothermodynamics", by Ernst H. Hirschel
     """
+    check = [_is_pint_quantity(t) for t in [cp, R, mu]]
+    if not (all(check) or (not any(check))):
+        raise ValueError(
+            "Detected a mix of units and dimensionless quantities."
+            " The evaluation can't proceed. Please, check the"
+            " dimensions of the provided quantities."
+        )
+    is_pint = any(check)
+    if _is_pint_quantity(cp):
+        cp = cp.to("J / kg / K").magnitude
+    if _is_pint_quantity(mu):
+        mu = mu.to("kg / m / s").magnitude
+    if _is_pint_quantity(R):
+        R = R.to("J / (kg * K)").magnitude
+
     # eq (4.18)
-    return (cp + 5.0 / 4.0 * R) * mu
+    k = (cp + 5.0 / 4.0 * R) * mu
+
+    if is_pint:
+        k *= _parse_pint_units("W / (m * K)")
+    return k
 
 
 @check_T
@@ -87,14 +189,39 @@ def thermal_conductivity_hansen(T):
     -------
     k : float or array_like
         Thermal conductivity of the gas [W / (m * K)]
+    
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from pygasflow.atd import thermal_conductivity_hansen
+    >>> thermal_conductivity_hansen(300)
+    np.float64(0.0251357567)
+
+    Pint quantities can be used as well:
+
+    >>> import pint
+    >>> import pygasflow
+    >>> ureg = pint.UnitRegistry()
+    >>> pygasflow.defaults.pint_ureg = ureg
+    >>> thermal_conductivity_hansen(300 * ureg.K)
+    <Quantity(0.0251357567, 'watt / meter / kelvin')>
 
     References
     ----------
 
     "Basic of Aerothermodynamics", by Ernst H. Hirschel
     """
+    is_pint = _is_pint_quantity(T)
+    if is_pint:
+        T = T.to("K").magnitude
+
     # eq (4.20)
-    return 1.993e-03 * T**1.5 / (T + 112.0)
+    k = 1.993e-03 * T**1.5 / (T + 112.0)
+
+    if is_pint:
+        k *= _parse_pint_units("W / (m * K)")
+    return k
 
 
 @check_T
@@ -110,12 +237,33 @@ def thermal_conductivity_power_law(T):
     -------
     k : float or array_like
         Thermal conductivity of the gas [W / (m * K)]
+    
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from pygasflow.atd import thermal_conductivity_power_law
+    >>> thermal_conductivity_power_law(300)
+    np.float64(0.0251985236)
+
+    Pint quantities can be used as well:
+
+    >>> import pint
+    >>> import pygasflow
+    >>> ureg = pint.UnitRegistry()
+    >>> pygasflow.defaults.pint_ureg = ureg
+    >>> thermal_conductivity_power_law(300 * ureg.K)
+    <Quantity(0.0251985236, 'watt / meter / kelvin')>
 
     References
     ----------
 
     "Basic of Aerothermodynamics", by Ernst H. Hirschel
     """
+    is_pint = _is_pint_quantity(T)
+    if is_pint:
+        T = T.to("K").magnitude
+
     is_scalar = False
     if not isinstance(T, np.ndarray):
         is_scalar = True
@@ -128,4 +276,7 @@ def thermal_conductivity_power_law(T):
     v[~idx] = 34.957e-05 * T[~idx]**0.75
     if is_scalar:
         return v[0]
+
+    if is_pint:
+        v *= _parse_pint_units("W / (m * K)")
     return v
--- tests/atd/test_thermal_conductivity.py
+++ tests/atd/test_thermal_conductivity.py
@@ -3,41 +3,184 @@ from pygasflow.atd.thermal_conductivity import (
     thermal_conductivity_power_law, thermal_conductivity_hansen,
     thermal_conductivity_chapman_enskog, thermal_conductivity_eucken
 )
+import pint
+import pytest
+import pygasflow
 
 
-def test_thermal_conductivity_power_law():
-    assert np.isclose(
-        thermal_conductivity_power_law(50), 0.004786)
-    assert np.isclose(
-        thermal_conductivity_power_law(350), 0.0282868891002169)
-    assert np.allclose(
-        thermal_conductivity_power_law([50, 100, 200, 300, 500, 1000]),
-        np.array([0.004786  , 0.009572  , 0.019144  , 0.02519852,
-            0.03696253, 0.06216331]))
+ureg = pint.UnitRegistry()
+pygasflow.defaults.pint_ureg = ureg
+W = ureg.W
+m = ureg.m
+s = ureg.s
+K = ureg.K
+J = ureg.J
+kg = ureg.kg
 
 
-def test_thermal_conductivity_hansen():
-    assert np.isclose(thermal_conductivity_hansen(50), 0.004349579675632066)
-    assert np.isclose(thermal_conductivity_hansen(350), 0.028246678681213885)
-    assert np.allclose(
-        thermal_conductivity_hansen([50, 100, 200, 300, 500, 1000]),
-        np.array([0.00434958, 0.00940094, 0.01806748, 0.02513576,
-            0.03640918, 0.05667643]))
+@pytest.mark.parametrize("use_pint", [False, True])
+def test_thermal_conductivity_power_law(use_pint):
+    T1 = 50
+    T2 = 350
+    T3 = np.array([50, 100, 200, 300, 500, 1000])
+
+    expected_r1 = 0.004786
+    expected_r2 = 0.0282868891002169
+    expected_r3 = np.array([0.004786  , 0.009572  , 0.019144  , 0.02519852,
+            0.03696253, 0.06216331])
+
+    if use_pint:
+        T1, T2, T3 = [t * K for t in [T1, T2, T3]]
+
+    r1 = thermal_conductivity_power_law(T1)
+    r2 = thermal_conductivity_power_law(T2)
+    r3 = thermal_conductivity_power_law(T3)
+
+    if use_pint:
+        units = set(r.units for r in [r1, r2, r3])
+        assert len(units) == 1
+        assert units.pop() == W / (m * K)
+        r1, r2, r3 = [r.magnitude for r in [r1, r2, r3]]
+
+    assert np.isclose(r1, expected_r1, rtol=1e-6, atol=1e-10)
+    assert np.isclose(r2, expected_r2, rtol=1e-6, atol=1e-10)
+    assert np.allclose(r3, expected_r3, rtol=1e-6, atol=1e-10)
 
 
-def test_thermal_conductivity_chapman_enskog():
-    assert np.isclose(
-        thermal_conductivity_chapman_enskog(50), 0.003708574505160782)
-    assert np.isclose(
-        thermal_conductivity_chapman_enskog(350), 0.022242098627654833)
-    assert np.isclose(
-        thermal_conductivity_chapman_enskog(300, gas="O2"),
-        0.019832044201669334)
-    assert np.allclose(
-        thermal_conductivity_chapman_enskog([50, 100, 200, 300, 500, 1000]),
-        np.array([0.00370857, 0.00752377, 0.01434195, 0.01983204,
-            0.02863668, 0.04547823]))
+@pytest.mark.parametrize("use_pint", [False, True])
+def test_thermal_conductivity_hansen(use_pint):
+    T1 = 50
+    T2 = 350
+    T3 = np.array([50, 100, 200, 300, 500, 1000])
 
-def test_thermal_conductivity_eucken():
-    assert np.isclose(
-        thermal_conductivity_eucken(1010, 288, 1.863e-05), 0.0255231)
+    expected_r1 = 0.004349579675632066
+    expected_r2 = 0.028246678681213885
+    expected_r3 = np.array([0.00434958, 0.00940094, 0.01806748, 0.02513576,
+            0.03640918, 0.05667643])
+
+    if use_pint:
+        T1, T2, T3 = [t * K for t in [T1, T2, T3]]
+
+    r1 = thermal_conductivity_hansen(T1)
+    r2 = thermal_conductivity_hansen(T2)
+    r3 = thermal_conductivity_hansen(T3)
+
+    if use_pint:
+        units = set(r.units for r in [r1, r2, r3])
+        assert len(units) == 1
+        assert units.pop() == W / (m * K)
+        r1, r2, r3 = [r.magnitude for r in [r1, r2, r3]]
+
+    assert np.isclose(r1, expected_r1, rtol=1e-6, atol=1e-10)
+    assert np.isclose(r2, expected_r2, rtol=1e-6, atol=1e-10)
+    assert np.allclose(r3, expected_r3, rtol=1e-6, atol=1e-10)
+
+
+@pytest.mark.parametrize("use_pint", [False, True])
+def test_thermal_conductivity_chapman_enskog(use_pint):
+    T1 = 50
+    T2 = 350
+    T3 = np.array([50, 100, 200, 300, 500, 1000])
+    T4 = 300
+
+    expected_r1 = 0.008809307530789567
+    expected_r2 = 0.05695432760614933
+    expected_r3 = np.array([0.00880931, 0.01639032, 0.03317209, 0.04943431,
+        0.077221 , 0.129541])
+    expected_r4 = 0.047112985675833156
+
+    if use_pint:
+        T1, T2, T3, T4 = [t * K for t in [T1, T2, T3, T4]]
+
+    r1 = thermal_conductivity_chapman_enskog(T1)
+    r2 = thermal_conductivity_chapman_enskog(T2)
+    r3 = thermal_conductivity_chapman_enskog(T3)
+    r4 = thermal_conductivity_chapman_enskog(T4, gas="N")
+
+    if use_pint:
+        units = set(r.units for r in [r1, r2, r3, r4])
+        assert len(units) == 1
+        assert units.pop() == W / (m * K)
+        r1, r2, r3, r4 = [r.magnitude for r in [r1, r2, r3, r4]]
+
+    assert np.isclose(r1, expected_r1, rtol=1e-6, atol=1e-10)
+    assert np.isclose(r2, expected_r2, rtol=1e-6, atol=1e-10)
+    assert np.allclose(r3, expected_r3, rtol=1e-6, atol=1e-10)
+    assert np.isclose(r4, expected_r4, rtol=1e-6, atol=1e-10)
+
+
+
+def test_thermal_conductivity_chapman_enskog_gases():
+    pytest.raises(
+        ValueError,
+        lambda : thermal_conductivity_chapman_enskog(50, gas="air")
+    )
+    pytest.raises(
+        ValueError,
+        lambda : thermal_conductivity_chapman_enskog(50, gas="H2")
+    )
+
+    # verify that the data table doesn't contain redundant data, specifically:
+    # 1. the same numeric values are not used for different gases.
+    # 2. these gases are supported.
+    gases = ['N', 'O', 'Ar', 'He']
+    T = np.linspace(200, 1000, 10)
+    results = {g: thermal_conductivity_chapman_enskog(T, gas=g) for g in gases}
+
+    gases = set(gases)
+    for gas in gases:
+        other_gases = gases.difference([gas])
+        for k in other_gases:
+            assert not np.allclose(results[gas], results[k])
+
+
+@pytest.mark.parametrize("use_pint", [False, True])
+def test_thermal_conductivity_eucken(use_pint):
+    cp, R, mu = 1010, 288, 1.863e-05
+
+    if use_pint:
+        cp *= J / (kg * K)
+        mu *= kg / (m * s)
+        R *= J / (kg * K)
+
+    k = thermal_conductivity_eucken(cp, R, mu)
+
+    if use_pint:
+        assert k.units == W / (m * K)
+        k = k.magnitude
+
+    assert np.isclose(k, 0.0255231)
+
+def test_thermal_conductivity_eucken_errors():
+    # mix of units and unitless quantities -> error
+
+    cp, R, mu = 1010, 288, 1.863e-05
+
+    # mu is unitless
+    pytest.raises(
+        ValueError,
+        lambda: thermal_conductivity_eucken(
+            cp * J / (kg * K),
+            R * J / (kg * K),
+            mu
+        )
+    )
+    # cp is unitless
+    pytest.raises(
+        ValueError,
+        lambda: thermal_conductivity_eucken(
+            cp,
+            R * J / (kg * K),
+            mu * kg / (m * s)
+        )
+    )
+    # R is unitless
+    pytest.raises(
+        ValueError,
+        lambda: thermal_conductivity_eucken(
+            cp * J / (kg * K),
+            R,
+            mu * kg / (m * s)
+        )
+    )
+
